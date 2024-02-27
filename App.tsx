cbbe87793066805c011c208f8a18ce19bf5d1afa
tudunilesh3@gmail.com <nilesh.tudu@nirmitee.io>
2024-02-27 21:36:06 +0530

    final push

--- App.tsx
+++ App.tsx
@@ -1,118 +1,207 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
+  Alert,
   View,
+  FlatList,
+  Platform,
+  StatusBar,
+  SafeAreaView,
+  NativeModules,
+  useColorScheme,
+  TouchableOpacity,
+  NativeEventEmitter,
+  PermissionsAndroid,
 } from 'react-native';
+import BleManager from 'react-native-ble-manager';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {DeviceList} from './src/DeviceList';
+import {styles} from './src/styles/styles';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const BleManagerModule = NativeModules.BleManager;
+const BleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
+const App = () => {
+  const peripherals = new Map();
+  const [isScanning, setIsScanning] = useState(false);
+  const [connectedDevices, setConnectedDevices] = useState([]);
+  const [discoveredDevices, setDiscoveredDevices] = useState([]);
+  const handleGetConnectedDevices = () => {
+    BleManager.getBondedPeripherals([]).then(results => {
+      for (let i = 0; i < results.length; i++) {
+        let peripheral = results[i];
+        peripheral.connected = true;
+        peripherals.set(peripheral.id, peripheral);
+        setConnectedDevices(Array.from(peripherals.values()));
+      }
+    });
+  };
+  useEffect(() => {
+    BleManager.enableBluetooth().then(() => {
+      console.log('Bluetooth is turned on!');
+    });
+    BleManager.start({showAlert: false}).then(() => {
+      console.log('BleManager initialized');
+      handleGetConnectedDevices();
+    });
+    let stopDiscoverListener = BleManagerEmitter.addListener(
+      'BleManagerDiscoverPeripheral',
+      peripheral => {
+        peripherals.set(peripheral.id, peripheral);
+        setDiscoveredDevices(Array.from(peripherals.values()));
+      },
+    );
+    let stopConnectListener = BleManagerEmitter.addListener(
+      'BleManagerConnectPeripheral',
+      peripheral => {
+        console.log('BleManagerConnectPeripheral:', peripheral);
+      },
+    );
+    let stopScanListener = BleManagerEmitter.addListener(
+      'BleManagerStopScan',
+      () => {
+        setIsScanning(false);
+        console.log('scan stopped');
+      },
+    );
+    if (Platform.OS === 'android' && Platform.Version >= 23) {
+      PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      ).then(result => {
+        if (result) {
+          console.log('Permission is OK');
+        } else {
+          PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          ).then(result => {
+            if (result) {
+              console.log('User accepted');
+            } else {
+              console.log('User refused');
+            }
+          });
+        }
+      });
+    }
+    return () => {
+      stopDiscoverListener.remove();
+      stopConnectListener.remove();
+      stopScanListener.remove();
+    };
+  }, []);
+  const startScan = () => {
+    if (!isScanning) {
+      BleManager.scan([], 5, true)
+        .then(() => {
+          console.log('Scanning...');
+          setIsScanning(true);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  };
+  // pair with device first before connecting to it
+  const connectToPeripheral = peripheral => {
+    BleManager.createBond(peripheral.id)
+      .then(() => {
+        peripheral.connected = true;
+        peripherals.set(peripheral.id, peripheral);
+        setConnectedDevices(Array.from(peripherals.values()));
+        setDiscoveredDevices(Array.from(peripherals.values()));
+        console.log('BLE device paired successfully');
+      })
+      .catch(() => {
+        console.log('failed to bond');
+      });
+  };
+  // disconnect from device
+  const disconnectFromPeripheral = peripheral => {
+    BleManager.removeBond(peripheral.id)
+      .then(() => {
+        peripheral.connected = false;
+        peripherals.set(peripheral.id, peripheral);
+        setConnectedDevices(Array.from(peripherals.values()));
+        setDiscoveredDevices(Array.from(peripherals.values()));
+        Alert.alert(`Disconnected from ${peripheral.name}`);
+      })
+      .catch(() => {
+        console.log('fail to remove the bond');
+      });
+  };
   const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
+  // render list of bluetooth devices
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={[backgroundStyle, styles.container]}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View style={{paddingHorizontal: 20}}>
+        <Text
+          style={[
+            styles.title,
+            {color: isDarkMode ? Colors.white : Colors.black},
+          ]}>
+          React Native BLE Manager Tutorial
+        </Text>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.scanButton}
+          onPress={startScan}>
+          <Text style={styles.scanButtonText}>
+            {isScanning ? 'Scanning...' : 'Scan Bluetooth Devices'}
+          </Text>
+        </TouchableOpacity>
+        <Text
+          style={[
+            styles.subtitle,
+            {color: isDarkMode ? Colors.white : Colors.black},
+          ]}>
+          Discovered Devices:
+        </Text>
+        {discoveredDevices.length > 0 ? (
+          <FlatList
+            data={discoveredDevices}
+            renderItem={({item}) => (
+              <DeviceList
+                peripheral={item}
+                connect={connectToPeripheral}
+                disconnect={disconnectFromPeripheral}
+              />
+            )}
+            keyExtractor={item => item.id}
+          />
+        ) : (
+          <Text style={styles.noDevicesText}>No Bluetooth devices found</Text>
+        )}
+        <Text
+          style={[
+            styles.subtitle,
+            {color: isDarkMode ? Colors.white : Colors.black},
+          ]}>
+          Connected Devices:
+        </Text>
+        {connectedDevices.length > 0 ? (
+          <FlatList
+            data={connectedDevices}
+            renderItem={({item}) => (
+              <DeviceList
+                peripheral={item}
+                connect={connectToPeripheral}
+                disconnect={disconnectFromPeripheral}
+              />
+            )}
+            keyExtractor={item => item.id}
+          />
+        ) : (
+          <Text style={styles.noDevicesText}>No connected devices</Text>
+        )}
+      </View>
     </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
+};
 export default App;
